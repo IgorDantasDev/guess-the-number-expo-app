9370cb64e915732e39a8186782e61a8ecb993f12
Igor Dantas <igordiasdantas@outlook.com>
2024-02-04 00:40:34 -0300

    just to change the default branch

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá, Mundo!!</Text>
+      <Text>Aparece ai develop</Text>
       <StatusBar style="auto" />
     </View>
   );
